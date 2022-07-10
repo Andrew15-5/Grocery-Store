97c2393879c81fa38eece5fe2fdd4f286c0ccec2
Andrew Voynov <andrewvoynov.b@gmail.com>
2022-07-10 23:41:29 +0300

    Added ./public/ folder to website visibility scope
    Now the content of ./public/ folder is visible to a browser (and end user)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express"
+import path from "path"
 
 const app = express()
 const SERVER_PORT = 3000
+
+app.use(express.static(path.resolve("./public")))
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server started on port ${SERVER_PORT}`)
