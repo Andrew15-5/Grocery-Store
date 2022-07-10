05f12c5e5e345037ca2f1d8228d0c38d903aa291
Andreev_Dmitry <coolich.ya@gmail.com>
2022-07-10 23:29:08 +0300

    Added basic registration page

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ const SERVER_PORT = 3000
 
 app.use(express.static(path.resolve("./public")))
 
+app.get("/registration", function (request, response) {
+  response.sendFile(path.resolve("./public/registration.html"));
+});
+
 app.listen(SERVER_PORT, () => {
   console.log(`Server started on port ${SERVER_PORT}`)
 })
