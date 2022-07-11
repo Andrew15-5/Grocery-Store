4394b244969f223db0163e3eea3949eb563b2049
Andrew Voynov <andrewvoynov.b@gmail.com>
2022-07-11 07:02:27 +0300

    Added dummy functions for #3
    Added check_password(), generate_hash() to ./src/utils/hash.ts

---
+++ src/utils/hash.ts
@@ -0,0 +1,7 @@
+export async function check_password(password: string, hash: string): Promise<boolean> {
+  return true
+}
+
+export async function generate_hash(password: string): Promise<string> {
+  return ""
+}
