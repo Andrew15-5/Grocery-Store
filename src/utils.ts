9c48d4d2532b4bbacfb84bdebb17609b759237ec
Andrew Voynov <andrewvoynov.b@gmail.com>
2022-07-12 19:16:01 +0300

    Removed unused export of Pool in ./src/utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { Response } from "express-serve-static-core/index"
 import path from "path"
 import { Pool } from "pg"
 
-export { path, Pool, Request, Response }
+export { path, Request, Response }
 
 export const pool = new Pool()
 
