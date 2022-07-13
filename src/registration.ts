dbcc9458e01b05ad4b1feffbab489df6517f7139
Andrew Voynov <andrewvoynov.b@gmail.com>
2022-07-13 03:45:23 +0300

    Moved username & password validation to ./src/utils.ts
    Moved from ./src/registration.ts

--- src/registration.ts
+++ src/registration.ts
@@ -1,3 +1,4 @@
+import * as utils from "./utils"
 import { path, pool, Request, Response } from "./utils"
 import * as auth from "./utils/auth"
 import { generate_hash } from "./utils/hash"
@@ -18,8 +19,8 @@ namespace registration {
       typeof password !== "string" ||
       typeof repeat_password !== "string" ||
       password !== repeat_password ||
-      !is_username_valid(username) ||
-      !is_password_valid(password)) {
+      !utils.is_username_valid(username) ||
+      !utils.is_password_valid(password)) {
       return response.status(400).redirect("/registration")
     }
 
@@ -42,25 +43,6 @@ namespace registration {
     }
 
     auth.authenticate_user(username, response).status(200).redirect("/catalog")
-  }
-
-  function is_username_valid(password: string) {
-    const allowed_charset_and_length_restriction = /^[a-zA-Z0-9._-]{1,20}$/
-    if (allowed_charset_and_length_restriction.test(password)) return true
-    return false
-  }
-
-  function is_password_valid(password: string) {
-    // Allow any visible ASCII character; length must be in [10;1000]
-    const allowed_charset_and_length_restriction = /^[ -~]{10,1000}$/
-    const includes_letters = /[a-z]+/
-    const includes_LETTERS = /[A-Z]+/
-    const includes_digits = /[0-9]+/
-    if (allowed_charset_and_length_restriction.test(password) &&
-      includes_letters.test(password) &&
-      includes_LETTERS.test(password) &&
-      includes_digits.test(password)) return true
-    return false
   }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -21,3 +21,22 @@ export function make_sure_all_env_vars_are_set() {
   }
   if (error_occured) process.exit(1)
 }
+
+export function is_username_valid(password: string) {
+  const allowed_charset_and_length_restriction = /^[a-zA-Z0-9._-]{1,20}$/
+  if (allowed_charset_and_length_restriction.test(password)) return true
+  return false
+}
+
+export function is_password_valid(password: string) {
+  // Allow any visible ASCII character; length must be in [10;1000]
+  const allowed_charset_and_length_restriction = /^[ -~]{10,1000}$/
+  const includes_letters = /[a-z]+/
+  const includes_LETTERS = /[A-Z]+/
+  const includes_digits = /[0-9]+/
+  if (allowed_charset_and_length_restriction.test(password) &&
+    includes_letters.test(password) &&
+    includes_LETTERS.test(password) &&
+    includes_digits.test(password)) return true
+  return false
+}
