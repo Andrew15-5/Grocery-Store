66ad390cc1f340f8e91a9a6f21669e3d4eed5b20
dmitry andreev <coolich.ya@gmail.com>
2022-07-14 00:42:27 +0300

    Changed the transition from the page to its reload
    Updated:
      ./public/js/log_out.js

--- public/js/log_out.js
+++ public/js/log_out.js
@@ -1,7 +1,6 @@
 "use strict";
 const log_out_element = document.getElementById("log_out")
 log_out_element.onclick = () => {
-  document.getElementById("log_out").id = "log_in"
   document.cookie = "access_token=;Max-Age=0"
-  window.location.assign("/catalog")
+  window.location.reload()
 };
