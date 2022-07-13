8cb63b14cb43d585d40e19abdac321454b5ef7d4
dmitry andreev <coolich.ya@gmail.com>
2022-07-13 17:20:05 +0300

    Added display of products to "/catalog" page
    Updated:
    catalog.ts

--- src/catalog.ts
+++ src/catalog.ts
@@ -1,8 +1,20 @@
-import { path, Request, Response } from "./utils"
+import { pool, Request, Response } from "./utils"
 
 namespace catalog {
-  export function get(request: Request, response: Response) {
-    response.status(200).render("catalog.hbs")
+  export async function get(request: Request, response: Response) {
+    try{
+      const query_response = await pool.query('SELECT image, name, price, description FROM products ORDER BY name;')
+        for (let i = 0; i < query_response.rowCount; i++) {
+          const image_buffer = query_response.rows[i].image.toString('base64')
+          const image_src = `data:image/png;base64,${image_buffer}`
+          query_response.rows[i].image = image_src
+        }
+        response.render("catalog.hbs", { products: query_response.rows })
+    }
+    catch(error) {
+      response.status(500).redirect("/")
+      throw error
+    }
   }
 }
 
