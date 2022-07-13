107352f0c7abe30dc5c395a85bdd576f36a19864
dmitry andreev <coolich.ya@gmail.com>
2022-07-13 23:39:47 +0300

    Added auto switching tag on "/catalog" page
    Added:
      ./public/js/log_in.js
    Updated:
      ./public/js/log_out.js
      ./src/catalog.ts
      ./views/catalog.hbs

---
+++ public/js/log_in.js
@@ -0,0 +1,5 @@
+"use strict";
+const log_in_element = document.getElementById("log_in")
+log_in_element.onclick = () => {
+  window.location.assign("/login")
+};
--- src/catalog.ts
+++ src/catalog.ts
@@ -1,4 +1,5 @@
 import { pool, Request, Response } from "./utils"
+import { authenticate_user, is_user_authenticated } from "./utils/auth"
 
 namespace catalog {
   export async function get(request: Request, response: Response) {
@@ -10,8 +11,9 @@ namespace catalog {
         const image_src = `data:image/png;base64,${image_buffer}`
         query_response.rows[i].image = image_src
       }
+      const is_auth = is_user_authenticated(request)
       response.status(200).render("catalog.hbs", {
-        products: query_response.rows
+        products: query_response.rows, is_auth: is_auth
       })
     }
     catch (error) {
