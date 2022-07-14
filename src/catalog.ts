11360eba0b3cfa5e2b2ad0ddbfa3b2f920f2994c
Andrew Voynov <andrewvoynov.b@gmail.com>
2022-07-14 23:14:04 +0300

    Added purchase buttons on "/catalog" page
    Now "/catalog" preserves "ref" parameter in URL if the page is refreshed

--- src/catalog.ts
+++ src/catalog.ts
@@ -3,9 +3,11 @@ import * as auth from "./utils/auth"
 
 namespace catalog {
   export async function get(request: Request, response: Response) {
+    const { ref } = request.query
+
     try {
-      const query_response = await pool.query(
-        "SELECT image, name, price, description FROM products ORDER BY name;")
+      const query_response =
+        await pool.query("SELECT * FROM products ORDER BY name;")
       for (let i = 0; i < query_response.rowCount; i++) {
         const image_buffer = query_response.rows[i].image.toString("base64")
         const image_src = `data:image/png;base64,${image_buffer}`
@@ -13,13 +15,21 @@ namespace catalog {
       }
       const is_auth = auth.is_user_authenticated(request)
       response.status(200).render("catalog.hbs", {
-        products: query_response.rows, is_auth: is_auth
+        is_auth: is_auth,
+        products: query_response.rows
       })
     }
     catch (error) {
-      response.status(500).redirect("/catalog")
+      const suffix = (ref) ? "?ref=" + ref : ''
+      response.status(500).redirect("/catalog" + suffix)
       throw error
     }
+  }
+
+  export async function post(request: Request, response: Response) {
+    const { ref } = request.query
+    const suffix = (ref) ? "?ref=" + ref : ''
+    response.status(200).redirect("/catalog" + suffix)
   }
 }
 
