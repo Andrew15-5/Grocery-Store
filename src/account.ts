d1d6bde6d214ff7ab3b326c7ca07aa71862f7e53
dmitry andreev <coolich.ya@gmail.com>
2022-07-15 01:59:24 +0300

    Added transfer of user data to account page
    Updated:
      ./src/account.ts
      ./views/account.hbs

--- src/account.ts
+++ src/account.ts
@@ -1,8 +1,21 @@
-import { Request, Response } from "./utils"
+import { pool, Request, Response } from "./utils"
+import { get_username } from "./utils/auth"
+
 
 namespace account {
     export async function get(request: Request, response: Response) {
-        response.status(200).render("account.hbs")
+        try {
+            const username = get_username(request)
+            const query_response = await pool.query(
+                `SELECT * FROM users WHERE username = '${username}';`)
+            response.status(200).render("account.hbs", {
+                account: query_response.rows
+            })
+        }
+        catch (error) {
+            response.status(500).redirect("/registration")
+            throw error
+        }
     }
 
     export async function post(request: Request, response: Response) {
