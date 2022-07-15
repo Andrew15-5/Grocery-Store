b8dd6442ed3e65915f400178e47b69bb08dbe0dc
dmitry andreev <coolich.ya@gmail.com>
2022-07-15 14:30:45 +0300

    Added product data transfer to the page
    Updated:
      ./src/product.ts
      ./views/product.hbs

--- src/product.ts
+++ src/product.ts
@@ -2,7 +2,17 @@ import { path, pool, Request, Response } from "./utils"
 namespace product {
     export async function get(request: Request, response: Response) {
         try {
-            response.status(200).render("product.hbs")
+            const uri = request.params["product_uri"]
+            const query_response = await pool.query(
+                `SELECT * from products where uri = '${uri}';`)
+            for (let i = 0; i < query_response.rowCount; i++) {
+                const image_buffer = query_response.rows[i].image.toString("base64")
+                const image_src = `data:image/png;base64,${image_buffer}`
+                query_response.rows[i].image = image_src
+            }
+            response.status(200).render("product.hbs", {
+                products: query_response.rows
+            })
         }
         catch (error) {
             console.log(error)
