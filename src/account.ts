2664938b93074b0795071190a37022402ad50762
Andrew Voynov <andrewvoynov.b@gmail.com>
2022-07-15 13:53:24 +0300

    Integrated copied-in-clipboardA referral url

--- src/account.ts
+++ src/account.ts
@@ -1,24 +1,20 @@
 import { fetch, Request, Response } from "./utils"
 import * as auth from "./utils/auth"
 
-async function get_reward_balance(username: string) {
+async function get_reward_balance_and_referral_id(username: string) {
   const REF_APP_SERVER_PORT = process.env.REF_APP_SERVER_PORT as string
-  const get_referral_id_url =
-    `http://localhost:${REF_APP_SERVER_PORT}/user-reward-balance/${username}`
+  const get_referral_info_url =
+    `http://localhost:${REF_APP_SERVER_PORT}/user-referral-info/${username}`
+  const data = await fetch(get_referral_info_url)
 
-  const data = await fetch(get_referral_id_url)
   if (data.status === 200) {
-    return {
-      reward_balance: (await data.json()).reward_balance as number
-    }
+    return await data.json()
   }
-  else {
-    return {
-      reward_balance: '-',
-      error_message:
-        "Ошибка работы сервиса реферальных ссылок. " +
-        `Не удалось получить Бонусный баланс.`
-    }
+  return {
+    reward_balance: '-',
+    error_message:
+      "Ошибка работы сервиса реферальных ссылок. " +
+      `Не удалось получить Бонусный баланс.`
   }
 }
 
@@ -28,11 +24,19 @@ namespace account {
       const username = auth.get_username(request)
       if (!username) return response.status(401).redirect("/login")
 
-      const { reward_balance, error_message } =
-        await get_reward_balance(username)
+      const { referral_id, reward_balance, error_message } =
+        await get_reward_balance_and_referral_id(username)
+
+      const host = request.get("host")
+      const referral_url = `${host}/catalog?ref=${referral_id}`
+      const js_onclick_script =
+        `navigator.clipboard.writeText("${referral_url}");`
 
       response.status(200).render("account.hbs", {
         error_message: error_message,
+        js_onclick_script: js_onclick_script,
+        referral_id: referral_id,
+        referral_url: referral_url,
         reward_balance: reward_balance,
         username: username
       })
