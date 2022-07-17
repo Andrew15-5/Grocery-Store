0fcb34b9b45852fa855fc7cb95ada77c44aa20be
Andrew Voynov <andrewvoynov.b@gmail.com>
2022-07-17 16:26:24 +0300

    Sorted requests' order in ./src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,8 @@ app.use(express.static(path.resolve("./public")))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookie_parser())
 
+app.get("/account", account.get)
+
 app.get("/catalog", catalog.get)
 app.post("/catalog", catalog.post)
 
@@ -30,8 +32,6 @@ app.get("/product/:product_uri", product.get)
 
 app.get("/registration", registration.get)
 app.post("/registration", registration.post)
-
-app.get("/account", account.get)
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server started on port ${SERVER_PORT}`)
